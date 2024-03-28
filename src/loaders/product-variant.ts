d158128e7836d73241cafd6fbbdb48980623612c
eskoniarek <skoniarek93@gmail.com>
2024-03-28 15:29:51 +0100

    pv

--- src/loaders/product-variant.ts
+++ src/loaders/product-variant.ts
@@ -1,20 +1,20 @@
-export default async function () {
-    const imports = (await import("@medusajs/medusa/dist/api/routes/store/products/index")) as any;
+// export default async function () {
+//     const imports = (await import("@medusajs/medusa/dist/api/routes/store/products/index")) as any;
     
-    // Add your custom fields to the allowed fields
-    imports.allowedAdminProductVariantFields = [
-      ...imports.allowedAdminProductVariantFields,
-      "name",
-      "file_key",
-      "mime_type",
-    ];
+//     // Add your custom fields to the allowed fields
+//     imports.allowedAdminProductVariantFields = [
+//       ...imports.allowedAdminProductVariantFields,
+//       "name",
+//       "file_key",
+//       "mime_type",
+//     ];
     
-    // Add your custom fields to the default fields
-    imports.defaultAdminProductVariantFields = [
-      ...imports.defaultAdminProductVariantFields,
-      "name",
-      "file_key",
-      "mime_type",
-    ];
-  }
+//     // Add your custom fields to the default fields
+//     imports.defaultAdminProductVariantFields = [
+//       ...imports.defaultAdminProductVariantFields,
+//       "name",
+//       "file_key",
+//       "mime_type",
+//     ];
+//   }
   
